b5c68ceec40c377f2a3931250656a9ce0be9af5d
Sangeet Parashar <parasharsangeet976@outlook.com>
2023-10-02 23:50:15 +0530

    add: ping command

--- src/main/java/dev/aperture/Main.java
+++ src/main/java/dev/aperture/Main.java
@@ -1,9 +1,35 @@
 package dev.aperture;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import javax.security.auth.login.LoginException;
+
+import dev.aperture.commands.Ping;
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class Main
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws LoginException
     {
-        System.out.println("Hello world!");
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("BOT_TOKEN");
+
+        JDA jda = JDABuilder.createDefault(token).enableIntents(GatewayIntent.GUILD_MEMBERS)
+                .enableIntents(GatewayIntent.DIRECT_MESSAGES).setActivity(Activity.playing("Aperture")).build();
+
+        jda.updateCommands()
+                .addCommands(Commands.slash("ping", "Calculates the ping of the bot."),
+                        Commands.slash("serverinfo", "Display all information regarding the server"),
+                        Commands.slash("userinfo", "Display all information of an user").addOption(OptionType.USER,
+                                "user", "The user to get.", false),
+                        Commands.slash("tictactoe", "Play tictactoe.").addOption(OptionType.USER, "user",
+                                "The user with whom you want to play with.", true))
+                .queue();
+
+        jda.addEventListener(new Ping());
     }
 }
---
+++ src/main/java/dev/aperture/commands/Ping.java
@@ -0,0 +1,20 @@
+package dev.aperture.commands;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Ping extends ListenerAdapter
+{
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event)
+    {
+        if (event.getName().equals("ping"))
+        {
+            long time = System.currentTimeMillis();
+
+            event.reply("Pinging...")
+                    .flatMap(v -> event.getHook().editOriginalFormat("%d ms", System.currentTimeMillis() - time))
+                    .queue();
+        }
+    }
+}
